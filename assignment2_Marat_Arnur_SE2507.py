0ca09560d4fac656f5f899baf7b55657e0d89e7e
Axxelium <arnurmarat8@gmail.com>
2026-04-16 16:07:45 +0500

    # Task 1 Completed

--- assignment2_Marat_Arnur_SE2507.py
+++ assignment2_Marat_Arnur_SE2507.py
@@ -1,4 +1,20 @@
 import os
 import csv
-import pandas as pd
+import sys
 
+data_dir = 'data'
+
+# Task 1
+os.makedirs(data_dir, exist_ok=True)
+
+filepath = os.path.join(data_dir, 'students_scores.csv')
+
+if os.path.exists(filepath):
+    abs_path = os.path.abspath(filepath)
+    print(f"File found: {abs_path}")
+    print(f"Absolute path: {abs_path}")
+else:
+    print('Error: File not found')
+    sys.exit(1)
+
+# Task 2
